d2ffe14418f50056a3394e3c4ea9acb0e8e125c1
veera <cptn.n3mo.f4@gmail.com>
2026-02-16 14:08:51 +0530

    Update StepperMotor-Advanced.py

--- StepperMotor-Advanced.py
+++ StepperMotor-Advanced.py
@@ -1,1 +1,21 @@
+from machine import Pin
+import time
 
+ln1 = Pin(14, Pin.OUT)
+ln2 = Pin(4, Pin.OUT)
+ln3 = Pin(18, Pin.OUT)
+ln4 = Pin(19, Pin.OUT)
+
+list = [[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1],[1,0,0,0]]
+
+while True:
+    for i in list:
+        print(i)
+        ln1.value(i[0])
+        time.sleep_ms(50)
+        ln2.value(i[1])
+        time.sleep_ms(50)
+        ln3.value(i[2])
+        time.sleep_ms(50)
+        ln4.value(i[3])
+        time.sleep_ms(50)
